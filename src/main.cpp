177e5f32f4b75313ca21edd2a01be271071d6093
Nano <nanomata@gmail.com>
2016-04-22 16:02:01 -0600

    Separacion de TAg y Index de la siguiente linea

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,18 @@
 #include <cstring>
 #include <sstream>
 #include <bitset>
-
-
+#include <climits>
 
 
 using namespace std;
 
+unsigned int newline_TAG;
+unsigned int newline_INDEX;
+
 
 const char* hex_char_to_bin(char c)
 {
-    // TODO handle default / error
+ 
     switch(toupper(c))
     {
         case '0': return "0000";
@@ -46,18 +48,38 @@ string HEXtoBIN(const string&  hex)
     return bin;
 }
 
+unsigned int toInt(const std::string &s)
+{
+    unsigned int n = 0;
 
+    for (int i = 0; i < s.size(); i++) {
+	if(s[i]=='1' || s[i]=='0'){
+        n <<= 1;
+        n |= s[i] - '0';}
+    }
 
+    return n;
+}
 
 
 int main()
 {
-    ifstream myfile;
-    string line; //linea leida
-    char HEX[13]; //Linea en Hexadecimal
-    char Accion; // Si es S o L
-    char dir[8]; //los 8bits hex de la dir
-    char DIR[32]; //los bits (binario) de la dir
+    	ifstream myfile;
+    	string line; //linea leida
+    	char HEX[13]; //Linea en Hexadecimal
+    	char Accion; // Si es S o L
+    	char dir[8]; //los 8bits hex de la dir
+    	char DIR[32]; //los bits (binario) de la dir
+
+
+    	
+    	int length_INDEX=10;
+    	int length_OFFSET=4;
+	int length_TAG=32 - (length_INDEX + length_OFFSET);
+    	char TAG[length_TAG];
+    	string Stag;
+    	char INDEX[length_INDEX];
+    	string Sindex;
 
 	//for(cada linea!)
  
@@ -84,15 +106,37 @@ int main()
 	//cout << "s: " << s <<endl;
 	string BIN =HEXtoBIN(s);
 	strcpy(DIR, BIN.c_str());
+
+	
+	for(int i=0; i<=32; i++){
+
+	if(i <= length_OFFSET){}
+	if(i <= length_OFFSET+length_INDEX && i> length_OFFSET) {
+		INDEX	[i-length_OFFSET] 		 = DIR[i];
+		//cout << "INDEX ["<<i-length_OFFSET<<"]: "<< DIR[i]<<endl;
+		}
+	if(i > length_OFFSET+length_INDEX){
+		TAG	[i-(length_OFFSET+length_INDEX)] = DIR[i];
+		}
+		
+	}
+
+	Stag = TAG;
+	cout << "Stag:" << Stag << endl;
+        Sindex = INDEX;
+	cout << "Sindex:" << Sindex<<  endl;	
+	//Tag?//Index?
+	newline_TAG = toInt(Stag); 
+	cout << "newline_TAG: " << newline_TAG <<endl;
+	newline_INDEX = toInt(Sindex); 
+	cout << "newline_INDEX: " << newline_INDEX <<endl;
+
+	//newline_TAG
+	//newline_INDEX
+
 	
 
-	//Tag?
 
-	//Index?
-
-	//Offset?
-
-	//Asociatividad??
 
     return 0;
 }
