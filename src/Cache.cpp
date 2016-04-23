82d4e8c7516069ef30b936abacadac3678399947
David Martinez <davidm251008@gmail.com>
2016-04-23 12:26:51 -0600

    updated

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -3,6 +3,9 @@ Cache::Cache(){
 	this->blockamm =0;
 	this->sets =0;
 	this->blocksize =0;
+	vector<Bloque> setdir (this->sets, Bloque(this->blocksize,this->sets));
+	vector< vector<Bloque> > precache (blockamm, setdir);
+	this->cache = precache;
 }
 Cache::Cache(int set, int blocks, int blocksizes){
 	this->blockamm = blocks;
