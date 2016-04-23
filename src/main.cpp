76f2c0880e04ac62161190d558d5f3ffea204b94
Nano <nanomata@gmail.com>
2016-04-23 11:02:20 -0600

    Get_Data function

--- src/main.cpp
+++ src/main.cpp
@@ -61,49 +61,29 @@ unsigned int toInt(const std::string &s)
     return n;
 }
 
+void Get_Data(string line, int length_INDEX, int length_OFFSET){
 
-int main()
-{
-    	ifstream myfile;
-    	string line; //linea leida
-    	char HEX[13]; //Linea en Hexadecimal
+	int length_TAG=32 - (length_INDEX + length_OFFSET);
+	char HEX[13]; //Linea en Hexadecimal
     	char Accion; // Si es S o L
     	char dir[8]; //los 8bits hex de la dir
     	char DIR[32]; //los bits (binario) de la dir
-
-
-    	
-    	int length_INDEX=10;
-    	int length_OFFSET=4;
-	int length_TAG=32 - (length_INDEX + length_OFFSET);
     	char TAG[length_TAG];
-    	string Stag;
     	char INDEX[length_INDEX];
     	string Sindex;
+	string Stag;
+	string s; 
+	
 
-	//for(cada linea!)
- 
-	string s = ""; 
-        myfile.open("./../aligned.trace");
-        cout << endl;
-        getline (myfile, line);
 	strcpy(HEX, line.c_str());
-
-	//for(int i=0;i<13;i++){
-    	//	cout << i << " : "<< HEX[i] << endl;
-	//}
-
 	Accion = HEX[12];
-	//cout << Accion <<endl;
-	
-	
+		
 	for(int i=0;i<8;i++)
 	{
     		dir[i]=HEX[i];
 	}
 
 	s = string(dir);
-	//cout << "s: " << s <<endl;
 	string BIN =HEXtoBIN(s);
 	strcpy(DIR, BIN.c_str());
 
@@ -112,8 +92,8 @@ int main()
 
 	if(i <= length_OFFSET){}
 	if(i <= length_OFFSET+length_INDEX && i> length_OFFSET) {
-		INDEX	[i-length_OFFSET] 		 = DIR[i];
-		//cout << "INDEX ["<<i-length_OFFSET<<"]: "<< DIR[i]<<endl;
+		INDEX	[i-length_OFFSET] = DIR[i];
+
 		}
 	if(i > length_OFFSET+length_INDEX){
 		TAG	[i-(length_OFFSET+length_INDEX)] = DIR[i];
@@ -122,17 +102,30 @@ int main()
 	}
 
 	Stag = TAG;
-	cout << "Stag:" << Stag << endl;
-        Sindex = INDEX;
-	cout << "Sindex:" << Sindex<<  endl;	
+        Sindex = INDEX;	
 	//Tag?//Index?
 	newline_TAG = toInt(Stag); 
-	cout << "newline_TAG: " << newline_TAG <<endl;
 	newline_INDEX = toInt(Sindex); 
-	cout << "newline_INDEX: " << newline_INDEX <<endl;
+	
 
-	//newline_TAG
-	//newline_INDEX
+}
+
+
+
+int main()
+{
+    	ifstream myfile;
+    	string line; //linea leida}
+
+	string s = ""; 
+        myfile.open("./../aligned.trace");
+        
+	//for(cada linea!)
+        getline (myfile, line);
+	Get_Data(line, 10, 4); //linea, tamaño index, tamaño offset
+	
+    	cout << "newline_TAG: " << newline_TAG <<endl;
+	cout << "newline_INDEX: " << newline_INDEX <<endl;
 
 	
 
