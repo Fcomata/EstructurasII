ef7f1b68699a786905b75efc8ba07cf304ea39d9
David Martinez <davidm251008@gmail.com>
2016-05-01 19:27:41 -0600

    Documentado

--- include/Bloque.h
+++ include/Bloque.h
@@ -19,26 +19,64 @@
 #define BLOQUE_H
 typedef unsigned long u32;
 using namespace std;
+//! Clase Bloque
+/*! Esta clase implementa la estructura general de un bloque de memoria en el cache.
+*	Para este caso, se considera que todas las direcciones de memoria son de 32 bits,
+*	por lo tanto se utilizan unsigned long, o u32 para su abreviación.
+*	Este bloque posee una implementación utilizando el Valid Bit.
+*/
 class Bloque{
     public:
-        Bloque();
-        Bloque(int);
+	//! Método constructor sin parámetros.
+	/*! Inicializa un  bloque que contiene 0 direcciones de memoria,
+	*	la hace no valida y le asigna un tag de 0.
+	*/
+	Bloque();
+	//! Método contructor con parámetro int tamano.
+	/*! Inicializa un bloque que contine tamano direcciones de memoria.
+	*	hace el bloque no valido y le asigna un tag de 0.
+	*
+	*/
+	Bloque(int);
+	//! Método constructor de copia.
+	/*! Crea una copia de un bloque anteriormente establecido
+	*
+	*/
 	Bloque(const Bloque&);
+	//! Sobrecarga del operador "=".
+	//! Se sobrecarga el operador para llamar al constructor de copia utilizando "=".
 	Bloque &operator=(const Bloque&);
-        virtual ~Bloque();
+	//! Método Destructor.
+	//! Actualmente no implementado.
+	virtual ~Bloque();
+	//! Método settag.
+	/*! 
+	*	Recibe un parámetro u32 para asignarle un tag a la dirección de memoria actual.
+	*/
 	void settag(u32);
+	//! Método gettag.
+	/*!
+	*	Retorna el valor del tag que está asignado a este bloque.
+	*/
 	u32 gettag(void);
+	//! Método getvalid.
+	//! Retorna el valor del "Valid Bit" del bloque.
 	bool getvalid(void);
+	//! Método setvalid.
+	//! Recibe un parámetro bool para asignarle el estado al valid bit.
 	void setvalid(bool);
     protected:
 
     private:
-        int blocksize;
-        bool valid;
-        u32 tag;
-        u32 index;
-        u32 offset;
-        int getoffset(int);
+	//! Variable privada int blocksize.
+	//! Almacena la cantidad de direcciones de memoria que se utilizan en el cache.
+    int blocksize;
+	//! Variable privada bool valid.
+	//! Almacena el valor lógico del "Valid Bit".
+    bool valid;
+	//! Variable privada u32 tag.
+	//! Almacena el tag. Se utiliza un u32 para evitar problemas de conversión a otro tipo de dato.
+    u32 tag;
 
 };
 
--- src/Bloque.cpp
+++ src/Bloque.cpp
@@ -18,22 +18,16 @@ Bloque::Bloque(){
 	this->blocksize = 0;
 	this->valid = 0;
 	this->tag =0;
-	this->index =0;
-	this->offset =0;
 }
 Bloque::Bloque(const Bloque& other){
 	this->blocksize = other.blocksize;
 	this->valid = other.valid;
 	this->tag = other.tag;
-	this->index = other.index;
-	this->offset = other.offset;
 }
 Bloque::Bloque(int tamano){
 	this->blocksize = tamano;
 	this->valid = 0;
 	this->tag =0;
-	this->index =0;
-	this->offset =0;
 }
 
 Bloque &Bloque::operator=(const Bloque& other){
@@ -41,14 +35,10 @@ Bloque &Bloque::operator=(const Bloque& other){
 		this->blocksize = other.blocksize;
 		this->valid = other.valid;
 		this->tag = other.tag;
-		this->index = other.index;
-		this->offset = other.offset;
 	}
 	return *this;
 }
-int Bloque::getoffset(int value){
-    return log2(value);
-}
+
 void Bloque::settag(u32 tag){
 	this->tag = tag;
 }
